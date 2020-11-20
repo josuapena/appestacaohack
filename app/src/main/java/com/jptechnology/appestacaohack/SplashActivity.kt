1a7b89a36bf29c7759cbce786c9ba71c88b2615d
josuapena <josuapena@gmail.com>
2020-11-20 18:46:54 -0300

    Class number 3.

---
+++ app/src/main/java/com/jptechnology/appestacaohack/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.jptechnology.appestacaohack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Open loginActivity after 5 sec
+        Handler
+    }
+}
