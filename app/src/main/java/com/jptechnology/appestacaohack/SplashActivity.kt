167a7032cb24bdc0d89e0b016d6cbac41cfda4ca
josuapena <josuapena@gmail.com>
2020-11-21 17:00:16 -0300

    Waiting load time added.

--- app/src/main/java/com/jptechnology/appestacaohack/SplashActivity.kt
+++ app/src/main/java/com/jptechnology/appestacaohack/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.jptechnology.appestacaohack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,11 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         // Open loginActivity after 5 sec
-        Handler
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start intent
+            val mIntent = Intent(this, LoginActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 5000)
     }
 }
